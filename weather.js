89a0b37f7aa81f204dbc56ea56b2e0d0639e1c4c
xJosHubx <144180971+xJosHubx@users.noreply.github.com>
2023-10-13 01:20:33 -0400

    Update weather.js
    Updated API to include timezone and lat/lon locations per Meteo requirements.

--- weather.js
+++ weather.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export function getWeather(lat, lon, timezone) {
     axios
         .get(
-            "https://api.open-meteo.com/v1/forecast?hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timeformat=unixtime",
+            "https://api.open-meteo.com/v1/forecast?latitude=40.7143&longitude=-74.006&hourly=temperature_2m,apparent_temperature,precipitation,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timeformat=unixtime&timezone=America%2FNew_York",
             {
                 params: {
                     latitude: lat,
